8be958714fad2c85e1f6a93ff6a305b261d0a873
Yi-Tang Chen <tom900908@gmail.com>
2025-06-17 15:26:57 +0800

    fix another issue for opencv.cpp

--- sampleCode/src/opencv.cpp
+++ sampleCode/src/opencv.cpp
@@ -1,10 +1,10 @@
+// 檔案: src/opencv.cpp
 #include "opencv.hpp"
 #include <iostream>
 
+// 建構函式：指定使用 GStreamer 後端
 Camera::Camera(int deviceIndex) {
-    // 使用 cv::CAP_GSTREAMER 來匹配 cheese 的行為
     cap_.open(deviceIndex, cv::CAP_GSTREAMER);
-
     if (!cap_.isOpened()) {
         std::cerr << "Error: 無法開啟相機 #" << deviceIndex << std::endl;
     } else {
@@ -12,6 +12,7 @@ Camera::Camera(int deviceIndex) {
     }
 }
 
+// 解構函式：釋放相機資源 (只有一個！)
 Camera::~Camera() {
     if (cap_.isOpened()) {
         cap_.release();
@@ -19,22 +20,16 @@ Camera::~Camera() {
     }
 }
 
+// 檢查相機是否開啟
 bool Camera::isOpened() const {
     return cap_.isOpened();
 }
 
+// 讀取一幀畫面
 cv::Mat Camera::getFrame() {
     cv::Mat frame;
     if (cap_.isOpened()) {
-        cap_.read(frame); // 使用 .read()
+        cap_.read(frame);
     }
-    // 不在這裡印警告，讓主程式去判斷
     return frame;
-}
-
-Camera::~Camera() {
-    if (cap_.isOpened()) {
-        cap_.release();
-        std::cout << "Camera 已釋放" << std::endl;
-    }
 }
